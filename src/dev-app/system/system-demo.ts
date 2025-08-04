018467a8d8ec7fd8dfd7b04971ee39e2e75bf150
Andrew Seguin <andrewseguin@google.com>
2025-08-04 16:31:00 -0600

    docs: add typography to system demo page

--- src/dev-app/system/system-demo.ts
+++ src/dev-app/system/system-demo.ts
@@ -116,4 +116,22 @@ export class SystemDemo {
       ],
     },
   ];
+
+  protected readonly typographyRoles: string[] = [
+    'display-large',
+    'display-medium',
+    'display-small',
+    'headline-large',
+    'headline-medium',
+    'headline-small',
+    'title-large',
+    'title-medium',
+    'title-small',
+    'body-large',
+    'body-medium',
+    'body-small',
+    'label-large',
+    'label-medium',
+    'label-small',
+  ];
 }
